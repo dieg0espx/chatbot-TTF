be90dceeac76529431c0d09d375a42d64c69f81f
dieg0espx <espinosa9mx@gmail.com>
2024-11-03 23:18:25 -0800

    .

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -28,6 +28,11 @@ function Chat() {
     getData();
   }, []);
 
+  useEffect(()=>{
+    console.log(conversation);
+    
+  },[conversation])
+
 
   async function getData() {
     try {
@@ -180,13 +185,12 @@ function Chat() {
                   message.sender === 'user' ? 'bg-[#1fe0ba] text-white' : 'bg-gray-200 text-black'
                 }`}
               >
-                {/* {message.text.split('\n').map((textPart, idx) => (
+                {message.text.split('\n').map((textPart, idx) => (
                   <React.Fragment key={idx}>
                     {textPart}
                     <br />
                   </React.Fragment>
-                ))} */}
-                <p> {message.text} </p>
+                ))}                 
                 <p className={`${message.sender === 'user' ? 'text-right' : 'text-left'} text-[6px] lowercase mt-[2px] mb-[-3px]`}>
                   {message.timestamp.split('|')[1]}
                 </p>
