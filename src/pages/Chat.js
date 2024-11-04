35a4c8a202d6a3d21e5c67bc204dff12d0614e40
dieg0espx <espinosa9mx@gmail.com>
2024-11-03 23:15:04 -0800

    .

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -186,7 +186,7 @@ function Chat() {
                     <br />
                   </React.Fragment>
                 ))} */}
-                {message}
+                <p> {message.text} </p>
                 <p className={`${message.sender === 'user' ? 'text-right' : 'text-left'} text-[6px] lowercase mt-[2px] mb-[-3px]`}>
                   {message.timestamp.split('|')[1]}
                 </p>
