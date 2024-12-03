12f5ee2446578db9d6870f09e70983bc04845ae7
dieg0espx <espinosa9mx@gmail.com>
2024-12-02 22:50:22 -0800

    .

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -18,7 +18,7 @@ function Chat() {
     'Contact',
     'Quotation',
     'Prices',
-    'About Us',
+    'About',
     'Delivery',
     'Other',
   ];
@@ -92,10 +92,63 @@ function Chat() {
   };
 
   const openAiUrl = process.env.REACT_APP_APIURL;
-  async function formattedAnswer(answer) {
-    const prompt = 'Format this JSON so people can understand: ' + answer;
+  async function rawAnswer(question, data) {
+    const prompt = `
+    You are an expert assistant. Answer the question below intelligently:
+    - If the question is directly related to the provided data, use the relevant parts of the data to answer.
+    - If the question is not related to the data, respond based on general knowledge and ignore the provided data.
+    - Do not include programming terms like JSON, keys, or objects in your response.
+    - Use bullet points or short sentences for clarity and readability.
+    - Highlight important elements or keys using **bold text** when necessary. For example: **key**.
+    - Do not include symbols like {}, [], :, <>, or anything that a non-technical person might find confusing.
+    - Always present the information in a way that a regular person can easily understand.
+    
+    Here is the data (use only if relevant):
+    ${JSON.stringify(data, null, 2)}
+    
+    Question: ${question}
+    `;
+    
+  
     try {
-      const response = await fetch(openAiUrl + 'generate-text', {
+      const response = await fetch(openAiUrl + '/generate-text', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt: prompt })
+      });
+  
+      if (!response.ok) {
+        throw new Error('Failed to generate text');
+      }
+  
+      const responseData = await response.json();
+      const generatedText = responseData.trimStart(); // Trim the leading spaces
+      console.log(generatedText);
+      addMessage('server', generatedText);
+    } catch (error) {
+      console.error('Error generating text:', error);
+      return 'Sorry, something went wrong while generating the response.';
+    }
+  }
+  
+
+  async function formattedAnswer(answer) {
+    const prompt = ` 
+    Organize the information in a clear and readable format using bullet points or short sentences. 
+    Use bold text when necessary to highlight important elements or keys. 
+    For example, format keys like this: **key**.
+    Do not use any programming terms like JSON, keys, or objects. 
+    Do not include any symbols like {}, [], :, <>, or anything that a non-technical person might find confusing. 
+    Instead, present the information as if you were explaining it to a regular person. 
+    
+    Here is the data to explain: ${answer}
+    `;
+    
+
+    try {
+      const response = await fetch(openAiUrl + '/generate-text', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -129,40 +182,78 @@ function Chat() {
     }
   };
 
-  const handleKeyDown = (event) => { 
-    if (event.key === 'Enter' && !event.shiftKey && currentMessage !== '') { 
-      event.preventDefault(); 
-      newUserMessage(); 
-    } 
-  }; 
+  // const handleKeyDown = (event) => { 
+  //   if (event.key === 'Enter' && !event.shiftKey && currentMessage !== '') { 
+  //     event.preventDefault(); 
+  //     newUserMessage(); 
+  //   } 
+  // }; 
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter' && !event.shiftKey && currentMessage.trim() !== '') {
+      event.preventDefault(); // Prevent default behavior
+      addMessage('user', currentMessage); // Add user message to the conversation
+      rawAnswer(currentMessage, data); // Pass the currentMessage and data to rawAnswer
+      setCurrentMessage(''); // Clear the input field
+    }
+  };
+
+  
   
   const newUserMessage = () => { 
     addMessage('user', currentMessage); 
     setCurrentMessage(''); 
   };
 
+  const getEmojiForOption = (option) => {
+    switch (option) {
+      case 'Products':
+        return '📦'; // Box emoji
+      case 'Contact':
+        return '📞'; // Telephone emoji
+      case 'Quotation':
+        return '💬'; // Speech balloon emoji
+      case 'Prices':
+        return '💲'; // Dollar emoji
+      case 'About':
+        return 'ℹ️'; // Info emoji
+      case 'Delivery':
+        return '🚚'; // Delivery truck emoji
+      case 'Other':
+        return '❓'; // Question mark emoji
+      default:
+        return '❔'; // Default emoji
+    }
+  };
+
+  function parseBoldText(text) {
+    const parts = text.split(/(\*\*.*?\*\*)/); // Split text into parts, keeping bold markers
+    return parts.map((part, index) =>
+      part.startsWith('**') && part.endsWith('**') ? (
+        <strong key={index}>{part.slice(2, -2)}</strong> // Remove ** and wrap in <strong>
+      ) : (
+        <React.Fragment key={index}>{part}</React.Fragment>
+      )
+    );
+  }
+
+
   return (
     <div className="chat-page h-screen overflow-hidden mx-auto">
       <Header />
-      <img
-        src={background}
-        className="w-[50%] max-w-[150px] fixed left-[50%] top-[30%] transform -translate-x-[45%] z-[2] opacity-[0.1]"
-      />
+      <img src={background} className="w-[50%] max-w-[150px] fixed left-[50%] top-[30%] transform -translate-x-[45%] z-[2] opacity-[0.05]" />
       <div className="container w-[98%] mx-auto z-1">
         <div className="bubbles relative h-[calc(100vh-110px)] p-[10px] z-[3] overflow-y-scroll rounded-t-lg pt-[70px]">
           {conversation.map((message, index) => (
-            <div
-              className={`row mb-[5px] flex ${message.sender === 'user' ? 'justify-end' : 'justify-start'}`}
-              key={index}
-            >
+            <div className={`row mb-[5px] flex ${message.sender === 'user' ? 'justify-end' : 'justify-start'}`} key={index}>
               <div
-                className={`${message.sender === 'user' ? 'user-bubble' : 'server-bubble'} max-w-[80%] w-fit min-w-[60px] py-[8px] px-[15px] rounded-lg text-left ${message.sender === 'user' ? 'bg-[#1fe0ba] text-white' : 'bg-gray-200 text-black'}`}>
+                className={`${message.sender === 'user' ? 'user-bubble' : 'server-bubble'} max-w-[80%] w-fit min-w-[60px] py-[8px] px-[15px] rounded-2xl text-left ${message.sender === 'user' ? 'bg-black text-white' : 'bg-gray-200 text-black'}`}>
                 {(typeof message.text === 'string' ? message.text.split('\n') : [message.text]).map((textPart, idx) => (
                   <React.Fragment key={idx}>
-                    {textPart}
+                    {parseBoldText(textPart)}
                     <br />
                   </React.Fragment>
-                ))}                 
+                ))}
                 <p className={`${message.sender === 'user' ? 'text-right' : 'text-left'} text-[6px] lowercase mt-[2px] mb-[-3px]`}>
                   {message.timestamp ? message.timestamp.split('|')[1] : ''}
                 </p>
@@ -171,22 +262,34 @@ function Chat() {
           ))}
 
           {/* Show main options if `showOptions` is true and no category selected */}
-          {showOptions && !selectedCategory && (
+          {showOptions && (
             <div className="options-container my-4">
-              <p className="text-gray-600 text-left">Please select an option below:</p>
-              <div className="grid grid-cols-2 gap-2 mt-2">
-                {options.map((option, index) => (
-                  <button
-                    key={index}
-                    onClick={() => handleOptionClick(option)}
-                    className="bg-gray-200 text-gray-700 py-1 px-3 rounded hover:bg-blue-600 hover:text-white"
-                  >
-                    {option}
-                  </button>
-                ))}
-              </div>
+
+              {!selectedCategory && (
+                <>
+                  <p className="text-gray-600 text-left">Please select an option below:</p>
+                  <div className="grid grid-cols-2 gap-2 mt-2">
+                    {options.map((option, index) => (
+                      <button
+                        key={index}
+                        onClick={() => handleOptionClick(option)}
+                        className="bg-gray-200 text-gray-700 py-3 px-3 rounded-2xl hover:bg-blue-600 hover:text-white text-left pl-[30px]"
+                      >
+                        <span role="img" aria-label="icon" className="mr-[10px]">
+                          {getEmojiForOption(option)}
+                        </span>
+                        {option}
+                      </button>
+                    ))}
+                  </div>
+                </>
+              )} 
             </div>
           )}
+
+
+
+
 
           {/* Show submenu options if `showOptions` is true and a category is selected */}
           {showOptions && selectedCategory && submenuOptions.length > 0 && (
@@ -194,25 +297,44 @@ function Chat() {
               <p className="text-gray-600 text-left">Select an option:</p>
               <div className="grid grid-cols-2 gap-2 mt-2">
                 {submenuOptions.map((subOption, index) => (
-                  <button key={index} onClick={() => handleSubOptionClick(subOption)} className="bg-gray-200 text-gray-700 py-1 px-3 rounded hover:bg-blue-600 hover:text-white">
+                  <button key={index} onClick={() => handleSubOptionClick(subOption)} className="bg-gray-200 text-gray-700 py-3 px-3 rounded-2xl hover:bg-blue-600 hover:text-white">
                     {capitalize(subOption)}
                   </button>
                 ))}
               </div>
             </div>
           )}
+
+          <div className="flex justify-start" style={{display: !selectedCategory ? 'none':'flex'}}>
+            <button
+              onClick={() => {
+                setSelectedCategory(null); // Reset the selected category
+                setSubmenuOptions([]); // Clear submenu options
+                addMessage('server', 'You are back to the main menu.');
+              }}
+              className="bg-gray-200 text-gray-700 py-2 px-4 rounded-2xl hover:bg-red-600 hover:text-white mb-4"
+            >
+              🔙 Back
+            </button>
+          </div>
+
+
+
+
         </div>
         
         {/* Text area for user input */}
-        <div className="input-area h-[100px] fixed bottom-[10px] left-0 w-full bg-white z-[997]">
-          <textarea
-            onChange={(e) => setCurrentMessage(e.target.value)}
-            onKeyDown={handleKeyDown}
-            value={currentMessage}
-            placeholder="Ask me something ..."
-            className="w-[98%] h-full p-2 mx-auto resize-none outline-none rounded-lg border border-gray-300 text-[16px]"
-          />
-        </div>
+        {selectedCategory === 'Other' && (
+          <div className="input-area h-[100px] fixed bottom-[10px] left-0 w-full bg-white z-[997]">
+            <textarea
+              onChange={(e) => setCurrentMessage(e.target.value)}
+              onKeyDown={handleKeyDown}
+              value={currentMessage}
+              placeholder="Ask me something ..."
+              className="w-[98%] h-full p-2 mx-auto resize-none outline-none rounded-lg border border-gray-300 text-[16px]"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
