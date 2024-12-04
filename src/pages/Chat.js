fae4ec9b0a6c9d66aeb4a8c50ef523d5b45bf76a
dieg0espx <espinosa9mx@gmail.com>
2024-12-04 02:18:27 -0600

    .

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -4,6 +4,9 @@ import grayBubble from '../images/graybubble.png';
 import background from '../images/logo.png';
 import Header from '../components/Header';
 import { capitalize } from '../Utils';
+import icon from '../images/bot.png'
+import loader from '../images/loader.gif'
+
 
 function Chat() {
   const [currentMessage, setCurrentMessage] = useState('');
@@ -12,233 +15,237 @@ function Chat() {
   const [data, setData] = useState();
   const [selectedCategory, setSelectedCategory] = useState(null); // Track selected main category
   const [submenuOptions, setSubmenuOptions] = useState([]); // Track submenu options
+  const [isLoading, setIsLoading] = useState(false)
 
   const options = [
-    'Products',
-    'Contact',
-    'Quotation',
-    'About',
-    'Delivery',
-    'Other',
+      'Products',
+      'Contact',
+      'Quotation',
+      'About',
+      'Delivery',
+      'Other',
   ];
 
   useEffect(() => {
-    addMessage('server', 'Hello, how can I help you? Please select an option below:');
-    getData();
+      getData();
   }, []);
 
+  useEffect(() => {
+      window.scrollTo({
+          top: document.body.scrollHeight,
+          behavior: 'smooth', // Optional: for smooth scrolling
+      });
+  }, [conversation]);
 
   async function getData() {
-    try {
+      try {
       const information = await import('../data/data.json');  
       setData(information.categories);
-    } catch (error) {
+      } catch (error) {
       console.error('Error fetching data:', error);
-    }
+      }
   }
 
   const addMessage = (sender, text = '') => {
-    const newMessage = {
+      const newMessage = {
       sender: sender,
       timestamp: getTimeStamp(),
       text: String(text), // Ensure text is a string
-    };
-    setConversation((prev) => [...prev, newMessage]);
-    setCurrentMessage('');
+      };
+      setConversation((prev) => [...prev, newMessage]);
+      setCurrentMessage('');
+      setIsLoading(false)
   };
 
   const getTimeStamp = () => {
-    const timestamp = new Date();
-    const date = timestamp.toLocaleDateString();
-    const time = timestamp.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    return `${date}|${time}`;
+      const timestamp = new Date();
+      const date = timestamp.toLocaleDateString();
+      const time = timestamp.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      return `${date}|${time}`;
   };
 
   const handleOptionClick = (option) => {
-    console.log(data[option]);
-    setShowOptions(false);
-    setSelectedCategory(option); // Set the selected category
-    addMessage('user', option);
-    addMessage('server', `Got it! You’re interested in ${option}.`);
-    setTimeout(() => displaySubmenu(option), 500); // Display submenu after a short delay
+      console.log(data[option]);
+      setShowOptions(false);
+      setSelectedCategory(option); // Set the selected category
+      addMessage('user', option);
+      addMessage('server', `Got it! You’re interested in ${option}.`);
+      setTimeout(() => displaySubmenu(option), 500); // Display submenu after a short delay
   };
 
   const displaySubmenu = (option) => {
-    if (data && option) {
+      if (data && option) {
       const categoryData = data[option];
       if (categoryData) {
-        const submenu = [];
-        if (categoryData.items && Array.isArray(categoryData.items)) {
+          const submenu = [];
+          if (categoryData.items && Array.isArray(categoryData.items)) {
           submenu.push(...categoryData.items.map((item) => item.name));
-        } else {
+          } else {
           submenu.push(...Object.keys(categoryData));
-        }
-        console.log('Submenu options set:', submenu);
-        setSubmenuOptions(submenu);
+          }
+          console.log('Submenu options set:', submenu);
+          setSubmenuOptions(submenu);
 
-        if(submenu[0] =='phone'){
+          if(submenu[0] =='phone'){
           console.log('print contact option');
           const formattedString = "Phone: 778 898 5301\nEmail: info@ttfscaffolding.com\nAddress: 10979 Olsen Rd, Surrey, BC V3V 3S9, Canada";
           addMessage('server', formattedString);
-        } else {
+          } else {
           setTimeout(() => {
-            addMessage('server', 'Please select an option below to learn more:');
-            setShowOptions(true);
+              addMessage('server', 'Please select an option below to learn more:');
+              setShowOptions(true);
           }, 500);
-        }
+          }
       } else {
-        console.log('No data found for selected category:', option);
+          console.log('No data found for selected category:', option);
       }
-    } else {
+      } else {
       console.log('Data or option not found');
-    }
+      }
   };
 
   const openAiUrl = process.env.REACT_APP_APIURL;
   async function rawAnswer(question, data) {
-    const prompt = `
-    You are an expert assistant. Answer the question below intelligently:
-    - If the question is directly related to the provided data, use the relevant parts of the data to answer.
-    - If the question is not related to the data, respond based on general knowledge and ignore the provided data.
-    - Do not include programming terms like JSON, keys, or objects in your response.
-    - Use bullet points or short sentences for clarity and readability.
-    - Concrete answer.
-    - Highlight important elements or keys using **bold text** when necessary. For example: **key**.
-    - Do not include symbols like {}, [], :, <>, or anything that a non-technical person might find confusing.
-    - Always present the information in a way that a regular person can easily understand.
-    
-    Here is the data (use only if relevant):
-    ${JSON.stringify(data, null, 2)}
-    
-    Question: ${question}
-    `;
-    
-  
-    try {
-      const response = await fetch(openAiUrl + 'generate-text', {
-        method: 'POST',
-        headers: {
+      const prompt = `
+      You are an expert assistant. Answer the question below intelligently:
+      - If the question is directly related to the provided data, use the relevant parts of the data to answer.
+      - If the question is not related to the data, respond based on general knowledge and ignore the provided data.
+      - Do not include programming terms like JSON, keys, or objects in your response.
+      - Use bullet points or short sentences for clarity and readability.
+      - Concrete answer.
+      - Highlight important elements or keys using **bold text** when necessary. For example: **key**.
+      - Do not include symbols like {}, [], :, <>, or anything that a non-technical person might find confusing.
+      - Always present the information in a way that a regular person can easily understand.
+      
+      Here is the data (use only if relevant):
+      ${JSON.stringify(data, null, 2)}
+      
+      Question: ${question}
+      `;
+      
+      
+      try {
+          setIsLoading(true);
+          const response = await fetch(openAiUrl + 'generate-text', {
+          method: 'POST',
+          headers: {
           'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ prompt: prompt })
+          },
+          body: JSON.stringify({ prompt: prompt })
       });
   
       if (!response.ok) {
-        throw new Error('Failed to generate text');
+          setIsLoading(false)
+          throw new Error('Failed to generate text');
       }
   
       const responseData = await response.json();
       const generatedText = responseData.trimStart(); // Trim the leading spaces
       console.log(generatedText);
       addMessage('server', generatedText);
-    } catch (error) {
+      } catch (error) {
       console.error('Error generating text:', error);
       return 'Sorry, something went wrong while generating the response.';
-    }
+      }
   }
   
 
   async function formattedAnswer(answer) {
-        
-    const prompt = ` 
-    Organize the information in a clear and readable format using bullet points or short sentences. 
-    Use bold text when necessary to highlight important elements or keys. 
-    For example, format keys like this: **key**.
-    Do not use any programming terms like JSON, keys, or objects. 
-    Do not include any symbols like {}, [], :, <>, or anything that a non-technical person might find confusing. 
-    Instead, present the information as if you were explaining it to a regular person. 
-    
-    Here is the data to explain: ${answer}
-    `; 
+          
+      const prompt = ` 
+      Organize the information in a clear and readable format using bullet points or short sentences. 
+      Use bold text when necessary to highlight important elements or keys. 
+      For example, format keys like this: **key**.
+      Do not use any programming terms like JSON, keys, or objects. 
+      Do not include any symbols like {}, [], :, <>, or anything that a non-technical person might find confusing. 
+      Instead, present the information as if you were explaining it to a regular person. 
+      
+      Here is the data to explain: ${answer}
+      `; 
 
-    try {
+      try {
+      setIsLoading(true)
       const response = await fetch(openAiUrl + 'generate-text', {
-        method: 'POST',
-        headers: {
+          method: 'POST',
+          headers: {
           'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ prompt: prompt })
+          },
+          body: JSON.stringify({ prompt: prompt })
       });
       if (!response.ok) {
-        throw new Error('Failed to generate text');
+          setIsLoading(false)
+          throw new Error('Failed to generate text');
       }
       const data = await response.json(); // Parse response once
       console.log(data);
       addMessage('server', data);
-    } catch (error) {
+      } catch (error) {
       console.error('Error generating text:', error);
       return 'Sorry, something went wrong while generating the response.';
-    }
+      }
   }
 
   const handleSubOptionClick = async (subOption) => {
-    addMessage('user', subOption);
-    const categoryKey = selectedCategory.replace(/[^a-zA-Z]/g, '');
-    const categoryData = data[categoryKey];
-    let subOptionData;
-    if (categoryKey === 'Products') {
+      addMessage('user', subOption);
+      const categoryKey = selectedCategory.replace(/[^a-zA-Z]/g, '');
+      const categoryData = data[categoryKey];
+      let subOptionData;
+      if (categoryKey === 'Products') {
       subOptionData = categoryData.items.find(item => item.name === subOption);
-    } else {
+      } else {
       subOptionData = categoryData[subOption];
-    }
-    if (subOptionData) {
+      }
+      if (subOptionData) {
       formattedAnswer(JSON.stringify(subOptionData, null, 2));      
-    }
+      }
   };
 
-  // const handleKeyDown = (event) => { 
-  //   if (event.key === 'Enter' && !event.shiftKey && currentMessage !== '') { 
-  //     event.preventDefault(); 
-  //     newUserMessage(); 
-  //   } 
-  // }; 
-
   const handleKeyDown = (event) => {
-    if (event.key === 'Enter' && !event.shiftKey && currentMessage.trim() !== '') {
+      if (event.key === 'Enter' && !event.shiftKey && currentMessage.trim() !== '') {
       event.preventDefault(); // Prevent default behavior
       addMessage('user', currentMessage); // Add user message to the conversation
       rawAnswer(currentMessage, data); // Pass the currentMessage and data to rawAnswer
       setCurrentMessage(''); // Clear the input field
-    }
+      }
   };
 
   
-  
+
   const newUserMessage = () => { 
-    addMessage('user', currentMessage); 
-    setCurrentMessage(''); 
+      addMessage('user', currentMessage); 
+      setCurrentMessage(''); 
   };
 
   const getEmojiForOption = (option) => {
-    switch (option) {
+      switch (option) {
       case 'Products':
-        return '📦'; // Box emoji
+          return '📦'; // Box emoji
       case 'Contact':
-        return '📞'; // Telephone emoji
+          return '📞'; // Telephone emoji
       case 'Quotation':
-        return '💬'; // Speech balloon emoji
+          return '💬'; // Speech balloon emoji
       case 'Prices':
-        return '💲'; // Dollar emoji
+          return '💲'; // Dollar emoji
       case 'About':
-        return 'ℹ️'; // Info emoji
+          return 'ℹ️'; // Info emoji
       case 'Delivery':
-        return '🚚'; // Delivery truck emoji
+          return '🚚'; // Delivery truck emoji
       case 'Other':
-        return '❓'; // Question mark emoji
+          return '❓'; // Question mark emoji
       default:
-        return '❔'; // Default emoji
-    }
+          return '❔'; // Default emoji
+      }
   };
 
   function parseBoldText(text) {
-    const parts = text.split(/(\*\*.*?\*\*)/); // Split text into parts, keeping bold markers
-    return parts.map((part, index) =>
+      const parts = text.split(/(\*\*.*?\*\*)/); // Split text into parts, keeping bold markers
+      return parts.map((part, index) =>
       part.startsWith('**') && part.endsWith('**') ? (
-        <strong key={index}>{part.slice(2, -2)}</strong> // Remove ** and wrap in <strong>
+          <strong key={index}>{part.slice(2, -2)}</strong> // Remove ** and wrap in <strong>
       ) : (
-        <React.Fragment key={index}>{part}</React.Fragment>
+          <React.Fragment key={index}>{part}</React.Fragment>
       )
-    );
+      );
   }
 
 
@@ -265,70 +272,68 @@ function Chat() {
             </div>
           ))}
 
-          {/* Show main options if `showOptions` is true and no category selected */}
-          {showOptions && (
-            <div className="options-container my-4">
+          {isLoading ? (
+               <div className="loader">
+                   <img src={loader} className='w-[80px] m-auto'/>
+               </div>
+            ):(
+            <>
+              {/* Show main options if `showOptions` is true and no category selected */}
+              {showOptions && (
+                <div className="options-container my-4">
+                
+                  {!selectedCategory && (
+                    <>
+                      <p className="text-gray-600 text-left">Please select an option below:</p>
+                      <div className="grid grid-cols-2 gap-2 mt-2">
+                        {options.map((option, index) => (
+                          <button
+                            key={index}
+                            onClick={() => handleOptionClick(option)}
+                            className="bg-gray-200 text-gray-700 py-3 px-3 rounded-2xl hover:bg-blue-600 hover:text-white text-left pl-[30px]"
+                          >
+                            <span role="img" aria-label="icon" className="mr-[10px]">
+                              {getEmojiForOption(option)}
+                            </span>
+                            {option}
+                          </button>
+                        ))}
+                      </div>
+                    </>
+                  )} 
+                </div>
+              )}
 
-              {!selectedCategory && (
-                <>
-                  <p className="text-gray-600 text-left">Please select an option below:</p>
+              {/* Show submenu options if `showOptions` is true and a category is selected */}
+              {showOptions && selectedCategory && submenuOptions.length > 0 && (
+                <div className="submenu-container my-4">
+                  <p className="text-gray-600 text-left">Select an option:</p>
                   <div className="grid grid-cols-2 gap-2 mt-2">
-                    {options.map((option, index) => (
-                      <button
-                        key={index}
-                        onClick={() => handleOptionClick(option)}
-                        className="bg-gray-200 text-gray-700 py-3 px-3 rounded-2xl hover:bg-blue-600 hover:text-white text-left pl-[30px]"
-                      >
-                        <span role="img" aria-label="icon" className="mr-[10px]">
-                          {getEmojiForOption(option)}
-                        </span>
-                        {option}
+                    {submenuOptions.map((subOption, index) => (
+                      <button key={index} onClick={() => handleSubOptionClick(subOption)} className="bg-gray-200 text-gray-700 py-3 px-3 rounded-2xl hover:bg-blue-600 hover:text-white">
+                        {capitalize(subOption)}
                       </button>
                     ))}
                   </div>
-                </>
-              )} 
-            </div>
-          )}
-
-
-
-
-
-          {/* Show submenu options if `showOptions` is true and a category is selected */}
-          {showOptions && selectedCategory && submenuOptions.length > 0 && (
-            <div className="submenu-container my-4">
-              <p className="text-gray-600 text-left">Select an option:</p>
-              <div className="grid grid-cols-2 gap-2 mt-2">
-                {submenuOptions.map((subOption, index) => (
-                  <button key={index} onClick={() => handleSubOptionClick(subOption)} className="bg-gray-200 text-gray-700 py-3 px-3 rounded-2xl hover:bg-blue-600 hover:text-white">
-                    {capitalize(subOption)}
-                  </button>
-                ))}
+                </div>
+              )}
+            
+              {/* BACK BUTTON */}
+              <div className="flex justify-start" style={{ display: !selectedCategory ? 'none' : 'flex' }}>
+                <button
+                  onClick={() => {
+                    setSelectedCategory(null); // Reset the selected category
+                    setSubmenuOptions([]); // Clear submenu options
+                    setShowOptions(true); // Show main options again
+                    addMessage('server', 'You are back to the main menu.');
+                  }}
+                  className="bg-gray-200 text-gray-700 py-2 px-4 rounded-2xl hover:bg-red-600 hover:text-white mb-4"
+                >
+                  🔙 Back
+                </button>
               </div>
-            </div>
-          )}
-
-
-          {/* BACK BUTTON */}
-          <div className="flex justify-start" style={{ display: !selectedCategory ? 'none' : 'flex' }}>
-            <button
-              onClick={() => {
-                setSelectedCategory(null); // Reset the selected category
-                setSubmenuOptions([]); // Clear submenu options
-                setShowOptions(true); // Show main options again
-                addMessage('server', 'You are back to the main menu.');
-              }}
-              className="bg-gray-200 text-gray-700 py-2 px-4 rounded-2xl hover:bg-red-600 hover:text-white mb-4"
-            >
-              🔙 Back
-            </button>
-          </div>
-
-
-
-
-
+              </>
+            )}
         </div>
         
         {/* Text area for user input */}
